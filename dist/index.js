03f18a3d08ddc925c04e3c60e3f6d201e9a6330a
cinta <cintatafur@gmail.com>
2023-03-01 11:03:58 +0100

    🧪 BD(mongoose):connect

--- dist/index.js
+++ dist/index.js
@@ -5,9 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./class/server"));
 const usuario_1 = __importDefault(require("./routes/usuario"));
+//mongosee es para conectar BD
+const mongoose_1 = __importDefault(require("mongoose"));
 const server = new server_1.default();
 //Rutas de la app
 server.app.use("/user", usuario_1.default);
+//Conectar BD
+mongoose_1.default.connect('mongodb://localhost:27017/fotosgram');
+//Levantar Express
 server.start(() => {
     console.log(`Servidor corriendo en puerto${server.port}`);
 });
--- index.ts
+++ index.ts
@@ -1,11 +1,18 @@
 import Server from "./class/server";
 import userRouter from "./routes/usuario";
+//mongosee es para conectar BD
+import mongoose from 'mongoose';
 
 const server = new Server();
 
 //Rutas de la app
 server.app.use("/user", userRouter);
 
+//Conectar BD
+mongoose.connect('mongodb://localhost:27017/fotosgram'); 
+
+
+//Levantar Express
 server.start(() => {
   console.log(`Servidor corriendo en puerto${server.port}`);
 });
