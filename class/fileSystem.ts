31314c94b08e5556fc334774482cca83a6f89730
cinta <cintatafur@gmail.com>
2023-03-06 11:16:25 +0100

    ✅ Generar(uniqid):creo id unicos y sus instalaciones

--- class/fileSystem.ts
+++ class/fileSystem.ts
@@ -1,15 +1,42 @@
 import { FileUp } from "../interfaces/fileUp";
 import path from "path";
 import fs from "fs";
+// npm i uniqid
+// npm i --save-dev @types/uniqid
+import uniqid from 'uniqid';
 
 export default class FileSystem {
   constructor() {}
 
-  guardarImgTemporal(fiel: FileUp, userID: string) {
+  guardarImgTemporal(file: FileUp, userID: string) {
+   //------------------------------------------------CREAR CARPETAS---------------------------------
     const path = this.crearCarpetaUser(userID);
+  
+    //-----------------------------------------------CREAR NOMBRE ARCH-------------------------------
+    const nombreArch = this.generarNombreUnic(file.name);
+    console.log(file.name);
+    console.log(nombreArch);
+   
+  
   }
 
-  crearCarpetaUser(userID: string) {
+  /* el nombreOriginal viene del file */
+  private generarNombreUnic( nombreOriginal: string ){
+   //6.copy.jpg
+   //separo por puntos para tener las 3 posiciones
+   //necesito la ultima posicion
+   const nombreArr = nombreOriginal.split('.');
+   //aqui guardo la ultima pos
+   const extension = nombreArr[nombreArr.length -1];
+   console.log('la ultima posicion es: ' , extension);
+   //importo  npm i --save-dev @types/uniqid 
+   //npm i uniqid
+   const idUnic = uniqid();
+   
+   return `${idUnic}.${extension}`;
+  }
+
+  private crearCarpetaUser(userID: string) {
     // con __dirname toda la estructura hasta la carpeta del proyecto para ello import--> path from 'path';
     //path--> BackendFotogramServe\uploads
     const pathUser = path.resolve(__dirname, "../uploads/", userID);
